5d7934d58f7bc783abd6695d380800f703b0293b
cristianjs19 <cristianjs19@gmail.com>
2019-06-20 18:27:34 -0300

    Add exercise

---
+++ Intro/07 - almostIncreasingSequence/almostIncreasingSequence.php
@@ -0,0 +1,21 @@
+<?php 
+
+function almostIncreasingSequence($sequence)
+{
+    $count = 0;
+    for ($i = 0; $i < count($sequence) - 1; $i++) {
+        if ($sequence[$i] >= $sequence[$i + 1]) {
+            $count++;
+        }
+        if (($i - 1 >= 0) &&
+            (count($sequence) - 2 > $i) &&
+            ($sequence[$i - 1] >= $sequence[$i + 1]) &&
+            ($sequence[$i] >= $sequence[$i + 2])
+            ) {
+            return false;
+        }
+    }
+    return $count <= 1 ? true : false;
+}
+
+ ?>
